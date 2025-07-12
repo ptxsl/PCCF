275ba9c6b4566260fed21a74615a513745de2c9c
Angel Berlanas Vicente <aberlanas@disroot.org>
2025-07-12 13:30:40 +0200

    Ingles para David Anyadido

--- tools/criterio-to-json.sh
+++ tools/criterio-to-json.sh
@@ -2,8 +2,8 @@
 
 cat boe/temp-txt.txt | grep -v "^$" | grep -v "Criterios de evaluaci" | while read line; do
 
-
     rc=0
+
     echo $line| grep -q "^[1-9]"|| rc=1
     if [ $rc -eq 0 ]; then
         RAT=$(echo $line |cut -d '.' -f2-)
